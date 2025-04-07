939c2a8511009bace5d6d92c8a331c92df32f1bd
Anna Petrasova <kratochanna@gmail.com>
2025-04-07 17:11:51 -0400

    test

--- analysis1.py
+++ analysis1.py
@@ -1,19 +1,28 @@
 import grass.script as gs
 
+
 def get_coordinates(points):
     """Function to get coordinate pairs from a vector point layer.
     Do not modify."""
     data = gs.read_command("v.out.ascii", input=points, separator="comma").splitlines()
     return [[float(coor) for coor in point.split(",")[:2]] for point in data]
 
+
 # modify here
 # change function name
 def analysis1(elevation, points):
     """Traces a flow through an elevation model"""
     coordinates = get_coordinates(points)
-    gs.run_command("r.drain", input=elevation, output="drain", output="drain_raster", drain="drain_vector", start_coordinates=coordinates)
+    gs.run_command(
+        "r.drain",
+        input=elevation,
+        output="drain_raster",
+        drain="drain_vector",
+        start_coordinates=coordinates,
+    )
+
 
 if __name__ == "__main__":
     elevation = "elevation"
     points = "lagoon_points"
-    analysis(elevation=elevation, points=points)
+    analysis1(elevation=elevation, points=points)

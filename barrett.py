304236c8d042366882da762a1478d50989cbfaa1
mbarrett87 <michael.barrett@tamuk.edu>
2025-04-15 17:12:08 -0400

    Created activity

---
+++ barrett.py
@@ -0,0 +1,11 @@
+import grass.script as gs
+
+# modify here
+# change function name
+def topidx(elevation):
+    """Computes profile curvature"""
+    gs.run_command("r.topidx", input="elevation", output="TWI")
+
+if __name__ == "__main__":
+    elevation = "elevation"
+    topidx(elevation=elevation)
